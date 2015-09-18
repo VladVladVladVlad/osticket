85084ce4b8fa5e33b1ce309ad45c394b3d344f4e
VladVladVladVlad <vladimir.m@vladimirm-pc.dev.cbl.corp>
2015-09-18 17:58:32 +0300

    5.1
    -Добавлен Total

--- scripts/Main.ps1
+++ scripts/Main.ps1
@@ -6,20 +6,38 @@ $MySQLDatabase = 'osticket'
 $MySQLHost = 'localhost'
 $ConnectionString = "server=" + $MySQLHost + ";port=3306;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";database="+$MySQLDatabase
 $Query = 
-"select osticket.ost_staff.username as 'Staff Memebrs', count(*) as 'Open Tickets'
-from osticket.ost_staff inner join osticket.ost_ticket 
-on osticket.ost_staff.staff_id = osticket.ost_ticket.staff_id 
-and osticket.ost_ticket.status like 'open'
-and (
-	osticket.ost_staff.username like 'AntonT' or osticket.ost_staff.username like 'Igor' 
-    or osticket.ost_staff.username like 'Ildar.Sh'
-    or osticket.ost_staff.username like 'Juli'
-    or osticket.ost_staff.username like 'Pavel'
-    or osticket.ost_staff.username like 'SergeyK'
-    or osticket.ost_staff.username like 'Vladimir'
+"(
+	select osticket.ost_staff.username as 'Staff Memebrs', count(*) as 'Open Tickets'
+	from osticket.ost_staff inner join osticket.ost_ticket 
+	on osticket.ost_staff.staff_id = osticket.ost_ticket.staff_id 
+	and osticket.ost_ticket.status like 'open'
+	and (
+		osticket.ost_staff.username like 'AntonT' or osticket.ost_staff.username like 'Igor' 
+		or osticket.ost_staff.username like 'Ildar.Sh'
+		or osticket.ost_staff.username like 'Juli'
+		or osticket.ost_staff.username like 'Pavel'
+		or osticket.ost_staff.username like 'SergeyK'
+		or osticket.ost_staff.username like 'Vladimir'
+	)
+	group by osticket.ost_staff.staff_id
+	#order by osticket.ost_staff.username asc
 )
-group by osticket.ost_staff.staff_id
-order by osticket.ost_staff.username;"
+union all
+(
+	select 'Тotal', count(*)
+	from osticket.ost_staff inner join osticket.ost_ticket 
+	on osticket.ost_staff.staff_id = osticket.ost_ticket.staff_id 
+	and osticket.ost_ticket.status like 'open'
+	and (
+		osticket.ost_staff.username like 'AntonT' or osticket.ost_staff.username like 'Igor' 
+		or osticket.ost_staff.username like 'Ildar.Sh'
+		or osticket.ost_staff.username like 'Juli'
+		or osticket.ost_staff.username like 'Pavel'
+		or osticket.ost_staff.username like 'SergeyK'
+		or osticket.ost_staff.username like 'Vladimir'
+	)
+)
+order by 1;"
 
 
 Try {
