17edd92cc556f2e29a595badc08cdf1edd5cc503
VladVladVladVlad <vladimir.m@vladimirm-pc.dev.cbl.corp>
2015-09-18 18:02:11 +0300

    5.2

--- queries/open_main.sql
+++ queries/open_main.sql
@@ -16,7 +16,7 @@
 )
 union all
 (
-	select 'Тotal', count(*)
+	select '`Total', count(*)
 	from osticket.ost_staff inner join osticket.ost_ticket 
 	on osticket.ost_staff.staff_id = osticket.ost_ticket.staff_id 
 	and osticket.ost_ticket.status like 'open'
--- scripts/Main.ps1
+++ scripts/Main.ps1
@@ -24,7 +24,7 @@ $Query =
 )
 union all
 (
-	select 'Тotal', count(*)
+	select '_Total', count(*)
 	from osticket.ost_staff inner join osticket.ost_ticket 
 	on osticket.ost_staff.staff_id = osticket.ost_ticket.staff_id 
 	and osticket.ost_ticket.status like 'open'
