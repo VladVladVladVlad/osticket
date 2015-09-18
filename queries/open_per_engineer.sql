15f2ab021a8fa38c49b0c901bae8ff3970f3133c
VladVladVladVlad <vladimir.m@vladimirm-pc.dev.cbl.corp>
2015-09-18 17:10:03 +0300

    2
    -Удалить лишние имена
    -Отсортирвать в алфавитом порядке

--- queries/open_per_engineer.sql
+++ queries/open_per_engineer.sql
@@ -1,3 +1,14 @@
-select osticket.ost_staff.username, count(*) from osticket.ost_staff
-inner join osticket.ost_ticket 
-on osticket.ost_staff.staff_id = osticket.ost_ticket.staff_id and osticket.ost_ticket.status like 'open' group by osticket.ost_staff.staff_id;
+select osticket.ost_staff.username, count(*) 
+from osticket.ost_staff inner join osticket.ost_ticket 
+on osticket.ost_staff.staff_id = osticket.ost_ticket.staff_id 
+and osticket.ost_ticket.status like 'open'
+and (
+	osticket.ost_staff.username like 'AntonT' or osticket.ost_staff.username like 'Igor' 
+    or osticket.ost_staff.username like 'Ildar.Sh'
+    or osticket.ost_staff.username like 'Juli'
+    or osticket.ost_staff.username like 'Pavel'
+    or osticket.ost_staff.username like 'SergeyK'
+    or osticket.ost_staff.username like 'Vladimir'
+)
+group by osticket.ost_staff.staff_id
+order by osticket.ost_staff.username;
